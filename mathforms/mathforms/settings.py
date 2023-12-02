d43738633049176cf4f2272eb600be9c24cacc7a
dneprovumar@gmail.com <dneprovumar@gmail.com>
2023-12-02 21:19:43 +1300

    11 day

--- mathforms/mathforms/settings.py
+++ mathforms/mathforms/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'users',
 ]
 
 MIDDLEWARE = [
--- mathforms/mathforms/urls.py
+++ mathforms/mathforms/urls.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-from django.urls import path,include
-from . import views
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('signup', views.signup),
-    path('login', views.login),
-    path('test_token', views.test_token),
-    path("users/", include("users.urls")),
+    path('admin/', admin.site.urls)
 ]
